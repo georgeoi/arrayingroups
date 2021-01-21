d90e31ddf74f774322dc4779612dbf7df9cc20db
georgeoi <--list>
2021-01-21 16:57:50 -0300

    resuelto

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const chunkArrayInGroups = function(arr, size){
 
-    /* YOUR CODE HERE */
+    var mySize = size
+    var result = []
+    var element = []
 
+    for (i=0;i<arr.length; i++){
+        element.push(arr[i])
+        mySize--
+
+        if (mySize==0){
+            result.push(element)
+            mySize= size
+            element= []
+        }
+
+    }
+    if (element.length > 0){
+        result.push(element)
+    }
+    return result
 }
 
 module.exports = chunkArrayInGroups
